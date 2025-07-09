25e0d19808b61212d3bacac8b20198785d4df20c
UniqueSwagger <s.rahmanrahi15@gmail.com>
2025-07-09 21:02:45 +0600

    main function done

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,16 @@ public class Main {
         employees[1] = new HourlyEmployee("Bob", "Brown", "222-22-2222", 16.75, 42);
         employees[2] = new CommissionEmployee("Carol", "Jones", "333-33-3333", 10000, 0.06);
         employees[3] = new BasePlusCommissionEmployee("Dave", "Wilson", "444-44-4444", 5000, 0.04, 300);
+
+        for (Employee emp : employees) {
+            if (emp instanceof BasePlusCommissionEmployee) {
+                ((BasePlusCommissionEmployee) emp).increaseBaseSalaryBy10Percent();
+            }
+        }
+
+        for (Employee emp : employees) {
+            System.out.println(emp.toString());
+            System.out.printf("Earnings: $%.2f\n\n", emp.earnings());
+        }
     }
 }
