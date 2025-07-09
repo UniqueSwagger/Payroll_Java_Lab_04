5251660598433d852406daf6fa8afa10f690b242
UniqueSwagger <s.rahmanrahi15@gmail.com>
2025-07-09 20:51:04 +0600

    fixed in Hourly Employee class and written some test codes in main

--- src/HourlyEmployee.java
+++ src/HourlyEmployee.java
@@ -2,7 +2,7 @@ public class HourlyEmployee extends Employee{
     private double wage;
     private double hours;
 
-    public HourlyEmployee(String firstName,String lastName,String ssn){
+    public HourlyEmployee(String firstName,String lastName,String ssn,double wage,double hours){
         super(firstName, lastName, ssn);
         this.wage=wage;
         this.hours=hours;
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+    public static void main(String[] args){
+        Employee[] employees = new Employee[4];
+
+        employees[0] = new SalariedEmployee("Alice", "Smith", "111-11-1111", 800.00);
+        employees[1] = new HourlyEmployee("Bob", "Brown", "222-22-2222", 16.75, 42);
+        employees[2] = new CommissionEmployee("Carol", "Jones", "333-33-3333", 10000, 0.06);
+        employees[3] = new BasePlusCommissionEmployee("Dave", "Wilson", "444-44-4444", 5000, 0.04, 300);
+    }
 }
